8740a5a75e60786761961a1be4ee06e090f0164c
Duane Nykamp <nykamp@umn.edu>
2013-05-24 12:30:19 -0500

    Starting course setup.

--- micourses/admin.py
+++ micourses/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django import forms
 from django.db import models
-from micourses.models import CommentForCredit, QuestionStudentAnswer
+from micourses.models import CommentForCredit, QuestionStudentAnswer, Course, GradeLevel, GradeCategory, CourseGradeCategory, Module, ModuleAssessment, StudentAssessmentAttempt
 import reversion
 
 class QuestionStudentAnswerAdmin(reversion.VersionAdmin):
@@ -10,6 +10,25 @@ class QuestionStudentAnswerAdmin(reversion.VersionAdmin):
 class CommentForCreditAdmin(reversion.VersionAdmin):
     pass
 
+class ModuleAssessmentInline(admin.TabularInline):
+    model = ModuleAssessment
+
+class ModuleAdmin(admin.ModelAdmin):
+    inlines=[ModuleAssessmentInline]
+    
+class CourseGradeCategoryInline(admin.TabularInline):
+    model = CourseGradeCategory
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines=[CourseGradeCategoryInline]
+    
 
 admin.site.register(CommentForCredit,CommentForCreditAdmin)
 admin.site.register(QuestionStudentAnswer,QuestionStudentAnswerAdmin)
+
+# not use reversion yet
+admin.site.register(Course, CourseAdmin)
+admin.site.register(GradeLevel)
+admin.site.register(GradeCategory)
+admin.site.register(Module,ModuleAdmin)
+admin.site.register(StudentAssessmentAttempt)
--- micourses/models.py
+++ micourses/models.py
@@ -24,4 +24,67 @@ class QuestionStudentAnswer(models.Model):
 
     def __unicode__(self):
         return  "%s" % self.answer
- 
+
+
+class GradeLevel(models.Model):
+    grade = models.CharField(max_length=1, unique=True)
+    
+    def __unicode__(self):
+        return self.grade
+
+class GradeCategory(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __unicode__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Grade categories'
+
+# should this be a group instead?
+class Course(models.Model):
+    code = models.SlugField(max_length=50, unique=True)
+    name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=400,blank=True)
+    grade_categories = models.ManyToManyField(GradeCategory, through='CourseGradeCategory')
+
+    def __unicode__(self):
+        return self.name
+
+class CourseGradeCategory(models.Model):
+    course = models.ForeignKey(Course)
+    grade_category = models.ForeignKey(GradeCategory)
+    number_count_for_grade = models.IntegerField(default=0)
+    
+    class Meta:
+        verbose_name_plural = 'Course grade categories'
+
+class Module(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=400,blank=True)
+    course = models.ForeignKey(Course)
+    assessments = models.ManyToManyField('mitesting.Assessment', through='ModuleAssessment')
+    sort_order = models.FloatField(default=0.0)
+    
+    def __unicode__(self):
+        return self.name
+
+
+class ModuleAssessment(models.Model):
+    module=models.ForeignKey(Module)
+    assessment=models.ForeignKey('mitesting.Assessment')
+    initial_due_date=models.DateField()
+    final_due_date=models.DateField()
+    grade_category = models.ForeignKey(GradeCategory, blank=True, null=True)
+    points = models.IntegerField(default=0)
+    required_for_grade = models.ForeignKey(GradeLevel, blank=True, null=True)
+    required_to_pass = models.BooleanField()
+    max_number_attempts = models.IntegerField(default=1)
+    sort_order = models.FloatField(default=0.0)
+    
+
+class StudentAssessmentAttempt(models.Model):
+    user = models.ForeignKey(User)
+    module_assessment = models.ForeignKey(ModuleAssessment)
+    date = models.DateTimeField()
+    score = models.IntegerField()
