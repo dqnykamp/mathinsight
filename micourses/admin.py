57d5b7090ec8dbb63710e23d37860fdfabfb538c
Duane Q. Nykamp <nykamp@umn.edu>
2013-08-22 11:26:21 -0400

    Add a Courses with just Assessments to the admin

--- micourses/admin.py
+++ micourses/admin.py
@@ -39,6 +39,33 @@ class CourseThreadContentInline(admin.StackedInline):
         return super(CourseThreadContentInline, self).get_formset(request, obj,
                 **kwargs)
 
+def course_assessment_thread_content_form_factory(thread):
+    class RuntimeAssessmentCourseThreadContentForm(forms.ModelForm):
+        
+        thread_content = forms.ModelChoiceField\
+            (label="Thread content", queryset=ThreadContent.objects\
+                 .filter(section__thread=thread)\
+                 .filter(content_type__model='assessment')\
+                 .order_by('section__sort_order','sort_order'))
+ 
+        class Meta:
+            model = CourseThreadContent
+ 
+    return RuntimeAssessmentCourseThreadContentForm
+
+class CourseAssessmentThreadContentInline(admin.StackedInline):
+    model = CourseThreadContent
+    def queryset(self, request):
+        qs = super(CourseAssessmentThreadContentInline, self).queryset(request)
+        return qs.filter(thread_content__content_type__model='assessment')
+
+    def get_formset(self, request, obj=None, **kwargs):
+        if obj is not None:
+            self.form = course_assessment_thread_content_form_factory(obj.thread) # obj is a Course
+        return super(CourseAssessmentThreadContentInline, self)\
+            .get_formset(request, obj, **kwargs)
+
+
 class CourseAdmin(admin.ModelAdmin):
     inlines=[CourseAssessmentCategoryInline, CourseEnrollmentInline, CourseSkipDate]
     fieldsets = (
@@ -100,10 +127,35 @@ class CourseWithThreadContentAdmin(admin.ModelAdmin):
     save_on_top=True
 
 
+class CourseWithAssessmentThreadContent(Course):
+    class Meta:
+        proxy = True
+        verbose_name_plural = "Courses with assessment thread content"
+
+
+class CourseWithAssessmentThreadContentAdmin(admin.ModelAdmin):
+    inlines=[CourseAssessmentThreadContentInline,]
+    fieldsets = (
+        (None, {
+                'fields': ('code', 'name',  'short_name', 'semester',
+                           'description', 'thread', 'active')
+                }),
+        )
+    
+    class Media:
+        js = [
+            "%sjs/jquery-latest.js" % settings.STATIC_URL,
+            "%sjs/django_admin_collapsed_inlines.js" % settings.STATIC_URL,
+        ]
+
+    save_on_top=True
+
+
 admin.site.register(QuestionStudentAnswer,QuestionStudentAnswerAdmin)
 
 # not use reversion yet
 admin.site.register(Course, CourseAdmin)
 admin.site.register(CourseWithThreadContent, CourseWithThreadContentAdmin)
+admin.site.register(CourseWithAssessmentThreadContent, CourseWithAssessmentThreadContentAdmin)
 admin.site.register(GradeLevel)
 admin.site.register(AssessmentCategory)
