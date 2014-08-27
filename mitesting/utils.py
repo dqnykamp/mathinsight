ade4f5d323c62d2c1bb1385248e87c2b5f023c1b
Duane Nykamp <nykamp@umn.edu>
2014-08-26 19:32:02 -0500

    Customized condition command is iif not if.

--- mitesting/utils.py
+++ mitesting/utils.py
@@ -44,7 +44,7 @@ def return_sympy_global_dict(allowed_sympy_commands=[]):
                           'evalf': evalf_expression,
                           'index': index,
                           'sum': sum,
-                          'if': iif,
+                          'iif': iif,
                           'len': len,
                           }
     
