bedafa8bed57395dc0543c706aefed81daf9892c
Duane Nykamp <nykamp@umn.edu>
2017-09-30 15:23:37 -0500

    Upgrade Geogebra

--- static/geogebra/HTML5/5.0/web/deferredjs/71B2BBF1DBBA67EE4556BCBBA7EC0A07/6.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.web.runAsyncCallback6('DXb(1,null,{});_.gC=function(){return this.cZ};oai(Bk)(6);\n//# sourceURL=web-6.js\n')
--- static/geogebra/HTML5/5.0/web/js/workercheck.js
+++ static/geogebra/HTML5/5.0/web/js/workercheck.js
@@ -1,2 +1,1 @@
-//just a dummy code to call, to see web workers supported or not.
 self.addEventListener("message",function(e){self.postMessage(e.data)});
